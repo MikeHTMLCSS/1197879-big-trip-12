2679a7e6e51b5f135693fe2f37a97edfe5ea49a9
Mike-Cursor <63852043+Mike-Cursor@users.noreply.github.com>
2020-08-15 23:33:18 +0300

    пытаюсь чинить

--- src/render/day.js
+++ src/render/day.js
@@ -1,8 +1,4 @@
 import {RENDER_POSITION} from '../data/constant.js';
-const OPENED_NAMES = {
-  point: `point`,
-  editing: `editing`,
-};
 const renderDays = (renderFunction, DayElementComponent, PointElementComponent, EditingElementComponent, container, data) => {
   data.forEach(dayData => {
     renderFunction(container, new DayElementComponent(dayData).getElement(), RENDER_POSITION.beforeEnd);
@@ -11,18 +7,9 @@ const renderDays = (renderFunction, DayElementComponent, PointElementComponent, 
       let pointElement = new PointElementComponent(pointData).getElement();
       let editingElement = new EditingElementComponent(pointData).getElement();
       renderFunction(dayContainer, pointElement, RENDER_POSITION.beforeEnd);
-      let openElement = dayContainer.querySelector(`.event__rollup-btn`);
-      let opened = OPENED_NAMES.point;
-      openElement.addEventListener(`click`, function (evt) {
-        if (evt.which === 1) {
-          if (opened = OPENED_NAMES.point) {
-            dayContainer.replaceChild(editingElement, pointElement);
-            opened = OPENED_NAMES.editing;
-          } else {
-            dayContainer.replaceChild(pointElement, editingElement);
-            opened = OPENED_NAMES.point;
-          }
-        }
+      let openElement = pointElement.querySelector(`.event__rollup-btn`);
+      openElement.addEventListener(`click`, function () {
+        dayContainer.replaceChild(editingElement, pointElement);
       });
     });
   });
