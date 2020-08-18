df1cfb6887d39807d009bf374e69ed8afe4a621d
Mike-Cursor <63852043+Mike-Cursor@users.noreply.github.com>
2020-08-18 19:33:25 +0300

    Разбиваем рендеринг на отдельные методы

--- src/main.js
+++ src/main.js
@@ -3,5 +3,5 @@ import Trip from './presenter/trip.js';
 import {callMockGenerationFunctions} from './mock/call.js';
 const routeData = callMockGenerationFunctions()[0];
 const dayData = callMockGenerationFunctions()[1];
-let trip = new Trip();
-trip.renderSite(routeData, dayData);
+let trip = new Trip(routeData, dayData);
+trip.renderSite();
--- src/presenter/trip.js
+++ src/presenter/trip.js
@@ -10,16 +10,37 @@ import DayElementComponent from '../view/day_element.js';
 import PointElementComponent from '../view/point_element.js';
 import EditingElementComponent from '../view/editing_element.js';
 export default class Trip {
-  renderSite(routeData, dayData) {
+  constructor(routeData, dayData) {
+    this._routeData = routeData;
+    this._dayData = dayData;
+    this._informationElement = new InformationComponent(this._routeData);
+    this._navigationElement = new NavigationComponent();
+    this._filterFormElement = new FiterFormComponent();
+    this._sortFormElement = new SortFormComponent();
+    this._dayListElement = new DayListComponent();
+  }
+  renderInformation() {
     const tripMainElement = document.querySelector(`.trip-main`);
-    renderElement(tripMainElement, new InformationComponent(routeData).getElement(), RENDER_POSITION.afterBegin);
+    renderElement(tripMainElement, this._informationElement.getElement(), RENDER_POSITION.afterBegin);
+  }
+  renderMenu() {
     const tripMenuElement = document.querySelector(`.trip-main__trip-controls`);
-    renderElement(tripMenuElement, new NavigationComponent().getElement(), RENDER_POSITION.beforeEnd);
-    renderElement(tripMenuElement, new FiterFormComponent().getElement(), RENDER_POSITION.beforeEnd);
+    renderElement(tripMenuElement, this._navigationElement.getElement(), RENDER_POSITION.beforeEnd);
+    renderElement(tripMenuElement, this._filterFormElement.getElement(), RENDER_POSITION.beforeEnd);
+  }
+  renderRoute() {
     const tripEventsElement = document.querySelector(`.trip-events`);
-    renderElement(tripEventsElement, new SortFormComponent().getElement(), RENDER_POSITION.beforeEnd);
-    renderElement(tripEventsElement, new DayListComponent().getElement(), RENDER_POSITION.beforeEnd);
+    renderElement(tripEventsElement, this._sortFormElement.getElement(), RENDER_POSITION.beforeEnd);
+    renderElement(tripEventsElement, this._dayListElement.getElement(), RENDER_POSITION.beforeEnd);
+  }
+  renderEvents() {
     const tripDaysElement = document.querySelector(`.trip-days`);
-    renderDays(renderElement, DayElementComponent, PointElementComponent, EditingElementComponent, tripDaysElement, dayData);
+    renderDays(renderElement, DayElementComponent, PointElementComponent, EditingElementComponent, tripDaysElement, this._dayData);
+  }
+  renderSite() {
+    this.renderInformation();
+    this.renderMenu();
+    this.renderRoute();
+    this.renderEvents();
   }
 };
